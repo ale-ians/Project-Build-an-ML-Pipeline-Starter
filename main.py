3dad2b0ea41a9d03c5479fd5222ab7bf5c6884c7
Ian <istev13@wgu.edu>
2025-06-13 15:42:09 -0600

    adding train_random_forest to the pipeline and to main.py implement here

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ _steps = [
     "basic_cleaning",
     "data_check",
     "data_split",
-    #"train_random_forest",
+    "train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
@@ -115,7 +115,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(),"src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config['modeling']['val_size'],
+                    "stratify_by": config['modeling']['stratify_by'],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                    "output_artifact": 'random_forest_export',
+                }
+            )
             pass
 
         if "test_regression_model" in active_steps:
